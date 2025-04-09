b0ba66534c3afdd023772774d2d457dbfd62ad00
mayashah-tam <mayashah.tam@tamu.edu>
2025-04-09 01:13:02 -0500

    added in logic for getting user flavor profile preferences

---
+++ AggieWineGlass/AggieWineGlass/ViewModels/FlavorProfileSelectionModel.swift
@@ -0,0 +1,35 @@
+//
+//  FlavorProfileSelectionModel.swift
+//  AggieWineGlass
+//
+//  Created by Maya Shah on 4/9/25.
+//
+
+import Foundation
+import Combine
+
+class FlavorProfileSelectionModel: ObservableObject {
+    @Published var preferences = UserPreferences.shared
+
+    // function to toggle selection of categories
+    func toggleFlavorProfileSelection(flavorProfile: String) {
+        if preferences.flavorProfiles.contains(flavorProfile) {
+            preferences.flavorProfiles.remove(flavorProfile)
+        } else {
+            preferences.flavorProfiles.insert(flavorProfile)
+        }
+        print(preferences.flavorProfiles)
+    }
+
+    func selectAllFlavorProfiles() {
+        preferences.flavorProfiles = Set(WineDataInfo.shared.uniqueFlavorProfiles)
+        print(preferences.flavorProfiles)
+    }
+    
+    func clearAllFlavorProfiles() {
+        preferences.flavorProfiles.removeAll()
+        print(preferences.flavorProfiles)
+    }
+}
+
+
